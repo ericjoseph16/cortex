0167f10b22bebd3b3aca93d8331acb72bf7683b7
ericjoseph16 <ericjoseph16@gmail.com>
2025-11-13 18:14:27 -0500

    new branch created for unused add function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 
-// #[derive(Parser)]
-// struct Cli {
-//     pattern: String,
-//     path: std::path::PathBuf,
-// }
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 
 fn main() {
     // rename("/Users/ericjoseph/Documents/rust-projects/cortex/poem.txt");
@@ -26,23 +26,22 @@ fn main() {
     }
 }
 
-
-// move file to docs folder
-fn add_file_to_docs(source_path: &Path) -> Result<(), Box<dyn Error>> {
-    let doc_path = PathBuf::from("docs");
-    fs::create_dir_all(&doc_path)?;
+// move file to docs folder - UNUSED
+// fn add_file_to_docs(source_path: &Path) -> Result<(), Box<dyn Error>> {
+//     let doc_path = PathBuf::from("docs");
+//     fs::create_dir_all(&doc_path)?;
     
-    if let Some(filename) = source_path.file_name() {
+//     if let Some(filename) = source_path.file_name() {
 
-        let mut dest_path = doc_path;
-        dest_path.push(filename);
+//         let mut dest_path = doc_path;
+//         dest_path.push(filename);
 
-        fs::rename(source_path, dest_path)?;
+//         fs::rename(source_path, dest_path)?;
 
-        println!("Successfully added file: {:?}", filename);
+//         println!("Successfully added file: {:?}", filename);
 
-    } else {
-        eprintln!("Error: Could not get filename from {:?}", source_path);
-    }
-    Ok(())
-}
+//     } else {
+//         eprintln!("Error: Could not get filename from {:?}", source_path);
+//     }
+//     Ok(())
+// }

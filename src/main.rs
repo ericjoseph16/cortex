04966004b4bbaba426025a2cfcb927cdd872206a
ericjoseph16 <ericjoseph16@gmail.com>
2025-11-13 18:20:28 -0500

    deleted docs folder

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 
-#[derive(Parser)]
-struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
-}
+// #[derive(Parser)]
+// struct Cli {
+//     pattern: String,
+//     path: std::path::PathBuf,
+// }
 
 fn main() {
-
+    
 }
